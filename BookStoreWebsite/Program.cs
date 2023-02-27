9855517f75ff7b3d2b6ef5e40c40ff145c306e65
HamidEng <hamiddikhan@gmail.com>
2023-02-27 09:48:33 +0500

    Create console application

---
+++ BookStoreWebsite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoookStoreWebsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
